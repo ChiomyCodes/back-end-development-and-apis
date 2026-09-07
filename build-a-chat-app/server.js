3ac1d04121d76719011bcd93fce29c50810c314f
ChiomyCodes <chiomyrejoice981@gmail.com>
2026-09-07 09:17:36 +0000

    build chat app

--- build-a-chat-app/server.js
+++ build-a-chat-app/server.js
@@ -3,3 +3,67 @@ import fs from 'fs';
 import { WebSocketServer } from 'ws';
 
 const PORT = 3001;
+const server = http.createServer((req, res) => {
+      fs.readFile("./public/index.html", (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Error reading file");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+})
+
+
+
+const wss = new WebSocketServer({server})
+
+
+const broadcast = (message) => {
+  const data = JSON.stringify(message);
+
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
+
+
+
+wss.on("connection", (socket, req) => {
+    const username = new URL(req.url, "http://localhost").searchParams.get(
+  "username",
+);
+  console.log("Client connected");
+
+  
+   socket.username = username;
+
+  //  someone joined
+  broadcast({
+    type: "system",
+    text: `${username} has joined the chat`,
+  });
+
+  socket.on("message", (data) => {
+    const message = JSON.parse(data);
+
+   
+    broadcast({
+      type: "chat",
+      username: socket.username,
+      text: message.text,
+    });
+  });
+
+  socket.on("close", () => {
+  
+    broadcast({
+      type: "system",
+      text: `${socket.username} has left the chat`,
+    });
+  });
+});
+
+server.listen(PORT, () => console.log(`server is running at http://localhost:3001 `))
